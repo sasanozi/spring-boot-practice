1037aa48afd7d63f3ea04a9bb29591cce4cf6158
chocowein <chokoweinochoko@gmail.com>
2024-10-05 14:14:43 +0900

    hello2525

--- spring/src/main/java/com/example/sample1app/Sample1appApplication.java
+++ spring/src/main/java/com/example/sample1app/Sample1appApplication.java
@@ -11,7 +11,7 @@ public class Sample1appApplication {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello World";
+    return "Hello World2525";
   }
 
   public static void main(String[] args) {
